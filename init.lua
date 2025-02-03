3c486bb9eb7a7124209941299c82a825ab61e43e
jungai <juworaphol125@gmail.com>
2025-02-03 16:29:35 +0700

    update!

--- init.lua
+++ init.lua
@@ -800,11 +800,13 @@ require('lazy').setup({
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'onsails/lspkind.nvim',
     },
     config = function()
       -- See `:help cmp`
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
+      local lspkind = require 'lspkind'
       luasnip.config.setup {}
 
       cmp.setup {
@@ -876,6 +878,55 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+        },
+        formatting = {
+          format = lspkind.cmp_format {
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = {
+              -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+              -- can also be a function to dynamically calculate max width such as
+              -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+              menu = 50, -- leading text (labelDetails)
+              abbr = 50, -- actual suggestion item
+            },
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function(entry, vim_item)
+              -- ...
+              return vim_item
+            end,
+            symbol_map = {
+              Copilot = '',
+              Text = '󰉿',
+              Method = '󰆧',
+              Function = '󰊕',
+              Constructor = '',
+              Field = '󰜢',
+              Variable = '󰀫',
+              Class = '󰠱',
+              Interface = '',
+              Module = '',
+              Property = '󰜢',
+              Unit = '󰑭',
+              Value = '󰎠',
+              Enum = '',
+              Keyword = '󰌋',
+              Snippet = '',
+              Color = '󰏘',
+              File = '󰈙',
+              Reference = '󰈇',
+              Folder = '󰉋',
+              EnumMember = '',
+              Constant = '󰏿',
+              Struct = '󰙅',
+              Event = '',
+              Operator = '󰆕',
+              TypeParameter = '',
+            },
+          },
         },
       }
     end,

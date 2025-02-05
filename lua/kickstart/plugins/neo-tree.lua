5c4bc3e31bde4621d7f1ac22c635cdf653f0fa81
jungai <juworaphol125@gmail.com>
2025-02-05 19:20:37 +0700

    update neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -36,7 +36,35 @@ return {
     window = {
       position = 'float',
     },
+    commands = {
+      system_open = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        -- macOs: open file in default application in the background.
+        vim.fn.jobstart({ 'open', path }, { detach = true })
+        -- Linux: open file in default application
+        vim.fn.jobstart({ 'xdg-open', path }, { detach = true })
+
+        -- Windows: Without removing the file from the path, it opens in code.exe instead of explorer.exe
+        local p
+        local lastSlashIndex = path:match '^.+()\\[^\\]*$' -- Match the last slash and everything before it
+        if lastSlashIndex then
+          p = path:sub(1, lastSlashIndex - 1) -- Extract substring before the last slash
+        else
+          p = path -- If no slash found, return original path
+        end
+        vim.cmd('silent !start explorer ' .. p)
+      end,
+    },
     filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+      window = {
+        mappings = {
+          ['O'] = 'system_open',
+        },
+      },
       filtered_items = {
         visible = false, -- when true, they will just be displayed differently than normal items
         hide_dotfiles = false,
